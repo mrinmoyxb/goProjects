ca3865f253c80b4ff8e76eaea5b5c43145c95288
mrinmoyborah <mrinmoyborah26100@gmail.com>
2026-09-01 21:56:56 +0530

    added POST, PATCH, DELETE

--- goHTTPServer/main.go
+++ goHTTPServer/main.go
@@ -10,19 +10,33 @@ import (
 )
 
 type Task struct {
-	ID        string `json:"id"`
+	ID        int    `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
 
+type Response struct {
+	Message string `json:"message"`
+	Task    Task   `json:"task"`
+}
+
+type TaskResponse struct {
+	Message string `json:"message"`
+}
+
+type UpdateTask struct {
+	Title     *string `json:"title"`
+	Completed *bool   `json:"completed"`
+}
+
 var tasks = []Task{
 	{
-		ID:        "1",
+		ID:        1,
 		Title:     "Learn Go",
 		Completed: false,
 	},
 	{
-		ID:        "2",
+		ID:        2,
 		Title:     "Learn MongoDB",
 		Completed: false,
 	},
@@ -32,9 +46,110 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi!! from Go Server")
 }
 
+// GET
 func getTasksHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(tasks)
+}
+
+// POST
+func createTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task Task
+
+	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	tasks = append(tasks, task)
+	response := Response{
+		Message: "success",
+		Task:    task,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(response)
+}
+
+// PATCH
+func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	taskIDStr := chi.URLParam(r, "id")
+	taskID, err := strconv.Atoi(taskIDStr)
+	if err != nil {
+		http.Error(w, "invalid task ID", http.StatusBadRequest)
+		return
+	}
+
+	var req UpdateTask
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+
+	found := false
+	for index := range tasks {
+		if tasks[index].ID == taskID {
+			found = true
+			if req.Title != nil {
+				tasks[index].Title = *req.Title
+			}
+			if req.Completed != nil {
+				tasks[index].Completed = *req.Completed
+			}
+		}
+	}
+
+	if !found {
+		http.Error(w, "task not found", http.StatusNotFound)
+		return
+	}
+
+	response := TaskResponse{
+		Message: "success",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+	return
+}
+
+// DELETE
+func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	taskIDStr := chi.URLParam(r, "id")
+	taskID, err := strconv.Atoi(taskIDStr)
+	if err != nil {
+		http.Error(w, "invalid taskID format", http.StatusBadRequest)
+		return
+	}
+
+	found := false
+	for index := range tasks {
+		if tasks[index].ID == taskID {
+			tasks = append(tasks[:index], tasks[index+1:]...)
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		http.Error(w, "task not found", http.StatusNotFound)
+		return
+	}
+
+	response := TaskResponse{
+		Message: "success",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+// PUT
+func putTaskHandler(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func main() {
@@ -43,6 +158,8 @@ func main() {
 
 	app.Get("/", homeHandler)
 	app.Get("/tasks", getTasksHandler)
+	app.Post("/tasks", createTaskHandler)
+	app.Delete("/tasks/{id}", deleteTaskHandler)
 
 	address := ":" + strconv.Itoa(PORT)
 
